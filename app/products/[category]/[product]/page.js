fd9c815fb84e053deefe651630e4910482afd825
Bernardita Marfán <bmmarfan@gmail.com>
2024-09-25 02:24:47 -0300

    problem fix

--- app/products/[category]/[product]/page.js
+++ app/products/[category]/[product]/page.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import ItemDetailContainer from '@/components/products/ItemDetailContainer'
 
-export async function generateStaticParams() {
-    const baseUrl = process.env.VERCEL_URL
-    ? `http://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000'
-    const products = await fetch(`${baseUrl}/api/products`).then((res) => res.json())
-   
-    return products.map((product) => ({
-      category: product.category,
-      product: product.slug,
-    }))
-  }
 
   export async function generateMetadata({ params }) {
     return {
