887efc9ef2803ebae3799e4df35ae1c845d65b87
Bernardita Marfán <bmmarfan@gmail.com>
2024-09-25 01:36:54 -0300

    fix

--- config/firebase.js
+++ config/firebase.js
@@ -6,11 +6,11 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: "proyecto-nextjs-coder.firebaseapp.com",
+  projectId: "proyecto-nextjs-coder",
+  storageBucket: "proyecto-nextjs-coder.appspot.com",
+  messagingSenderId: "727875746644",
+  appId: "1:727875746644:web:cc3695f55d41149db626c1"
 };
 
 // Initialize Firebase
